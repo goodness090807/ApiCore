ffb2c67a2d42977dcfb95c3d91047bc1f35a3b88
Terry <b0972834939@gmail.com>
2023-07-06 20:52:59 +0800

    feat: 增加Sagger，和移除不必要的程式碼

--- Startup.cs
+++ Startup.cs
@@ -1,22 +1,19 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using APICore.Datas;
 using APICore.Repositorys;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using AutoMapper;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.IdentityModel.Tokens;
 using System.Text;
+using Microsoft.OpenApi.Models;
+using System.Reflection;
+using System.IO;
 
 namespace APICore
 {
@@ -94,11 +91,53 @@ namespace APICore
                     };
                 });
             #endregion
+
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc(
+                    // name: §ñÃö SwaggerDocument ªº URL ¦ì¸m¡C
+                    name: "v1",
+                    new OpenApiInfo
+                    {
+                        Version = "v1",
+                        Title = "My API V1",
+                        Description = "A simple example ASP.NET Core Web API",
+                        TermsOfService = new Uri("https://apicorepractice.herokuapp.com"),
+                        Contact = new OpenApiContact
+                        {
+                            Name = "Terry",
+                            Email = string.Empty,
+                            Url = new Uri("https://apicorepractice.herokuapp.com"),
+                        },
+                        License = new OpenApiLicense
+                        {
+                            Name = "Use under LICX",
+                            Url = new Uri("https://example.com/license"),
+                        }
+                    }
+                );
+                var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+                var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+                c.IncludeXmlComments(xmlPath);
+            });
+
+            
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            // Enable middleware to serve generated Swagger as a JSON endpoint.
+            app.UseSwagger();
+
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.),
+            // specifying the Swagger JSON endpoint.
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+            });
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
